0a010402a265364447c6101571a4da7079fa9ec7
Alex Lee <hyunho98@gmail.com>
2020-02-28 20:58:27 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,2 +1,14 @@
 class PigLatinizer
+
+
+  def initialize(words)
+    @words = words.split(" ")
+  end
+
+  def piglatinize
+    plwords = @words.collect do |word|
+      letters = word.split()
+      until letters.first() != /[aeiou]/
+
+
 end
