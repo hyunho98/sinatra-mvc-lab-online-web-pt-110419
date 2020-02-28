4e198b38aced772e059977215792d2deca35f96c
Alex Lee <hyunho98@gmail.com>
2020-02-28 21:21:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @piglatin = PigLatinizer.new(params[:user_phrase])
+    @piglatin = PigLatinizer.new()
     erb :user_input
   end
 end
